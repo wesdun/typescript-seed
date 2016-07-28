232fcdc62a062d316208bda11467edaf259e7e5a
Greater Sum Team <dev01@greatersum.com>
2016-07-28 15:36:23 -0400

    add coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-    plugins: ['karma-systemjs', 'karma-jasmine', 'karma-phantomjs-launcher'],
+    plugins: ['karma-systemjs', 'karma-jasmine', 'karma-phantomjs-launcher', 'karma-remap-istanbul', 'karma-coverage'],
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['systemjs', 'jasmine'],
@@ -38,15 +38,28 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*.js': ['coverage']
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
 
+    coverageReporter: {
+        type: 'json',
+        subdir: '.',
+        file: 'coverage-final.json'
+    },
 
+    remapIstanbulReporter: {
+      src: 'coverage/coverage-final.json',
+      reports: {
+        html: 'coverage'
+      },
+      timeoutNotCreated: 1000,
+      timeoutNoMoreFiles: 1000
+    },
     // web server port
     port: 9876,
 
